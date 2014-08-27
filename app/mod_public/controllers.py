5dddc485a7b216b44d159524b059f168f339f2fc
Shane Eller <shane.eller@gmail.com>
2014-08-27 16:19:08 -0400

    user cleanup

--- app/mod_public/controllers.py
+++ app/mod_public/controllers.py
@@ -1,7 +1,6 @@
 ## Import flask dependencies
 from flask import Blueprint, request, render_template, \
-            flash, g, session, redirect, url_for
-
+	flash, g, session, redirect, url_for
 # Import password / encryption helper tools
 from werkzeug import check_password_hash, generate_password_hash
 
@@ -13,6 +12,6 @@ mod_public = Blueprint('public', __name__, url_prefix='')
 
 
 # Set the route and accepted methods
-@mod_public.route('/', methods=['GET', 'POST'])
+@mod_public.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
